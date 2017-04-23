72ee569257c28638a3ee6005c9537a82af119966
Julius <julius.berger@gmail.com>
2017-04-23 02:19:36 +0200

    Menübar hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        AnchorPane root = FXMLLoader.load(getClass().getResource("components/root.fxml"));
+        VBox root = FXMLLoader.load(getClass().getResource("components/root.fxml"));
         Scene scene = new Scene(root);
 
         primaryStage.setResizable(false);
