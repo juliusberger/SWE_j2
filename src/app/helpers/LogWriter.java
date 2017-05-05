90b0e859310faccf029703b78f41cbef92d72d76
Michi Mößner <michi.moessner@gmx.de>
2017-05-05 23:18:38 +0200

    Mit Implementierung der LogWriter-Klasse begonnen

--- src/app/helpers/LogWriter.java
+++ src/app/helpers/LogWriter.java
@@ -1,18 +1,45 @@
 package app.helpers;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 /**
  * Erstellt von Michi am 05.05.2017.
  */
 public final class LogWriter
 {
-    private static String fileName;
+    private static String fileName = "";
+    private static BufferedWriter fileWriter = null;
+    private static boolean canWrite = false;
 
     // Klasse soll nicht-instanziierbar sein
-    private LogWriter()
-    {
+    private LogWriter(){
     }
 
     public static void writeLog(String toWrite){
+        if (canWrite && fileWriter != null)
+        {
+            try{
+                fileWriter.write(toWrite);
+                fileWriter.newLine();
+            }
+            catch (Exception ex){
+                //TODO MessageBox an User
+            }
+        }
+    }
+
+    private static boolean createLogFile(){
+        try{
+            fileWriter = new BufferedWriter(new FileWriter(fileName));
+            //TODO Startinformation in Datei schreiben (z.B. Logfile für Projekt "Test":)
+            return true;
+        }
+        catch (Exception ex) {
+            //TODO MessageBox als Info für User einfügen
+            System.out.println("Fehler beim Erstellen des Logfiles:\n" + ex.toString());
+            return false;
+        }
 
     }
 
@@ -22,6 +49,7 @@ public final class LogWriter
 
     public static void setFileName(String fileName){
         LogWriter.fileName = fileName;
+        canWrite = createLogFile();
     }
 
 }
--- src/app/helpers/XML.java
+++ src/app/helpers/XML.java
@@ -24,6 +24,7 @@ import java.util.List;
  */
 public final class XML
 {
+    // Klasse soll nicht-instanziierbar sein
     private XML()
     {
     }
