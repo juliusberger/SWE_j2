f38794430a794e895e1d0c89ba07c88ecc163a73
Michi Mößner <michi.moessner@gmx.de>
2017-06-03 22:49:45 +0100

    Input-Methode für FunctionalRequirements implementiert

--- src/app/models/implementation/Requirements/FunctionalRequirements.java
+++ src/app/models/implementation/Requirements/FunctionalRequirements.java
@@ -7,6 +7,7 @@ import app.models.interfaces.Requirements.I_FunctionalRequirements;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
+import java.util.ArrayList;
 
 /**
  * Created by Michi on 23.04.2017.
@@ -39,6 +40,10 @@ class FunctionalRequirements extends ObservableEntryHolder<I_FunctionalRequireme
 
     @Override
     public void importFromXML(XMLStreamReader xmlReader) throws XMLStreamException {
-
+        ArrayList<String> functionalRequirementsEntryArguments = new ArrayList<>();
+        functionalRequirementsEntryArguments.add(xmlReader.getAttributeValue(0));
+        functionalRequirementsEntryArguments.add(xmlReader.getAttributeValue(1));
+        functionalRequirementsEntryArguments.add(xmlReader.getAttributeValue(2));
+        this.addEntryWithProperties(functionalRequirementsEntryArguments);
     }
 }
