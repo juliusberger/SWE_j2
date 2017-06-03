809c3e0e18a00480366385b5e7b8f5bf0cb95b5c
Michi Mößner <michi.moessner@gmx.de>
2017-06-03 22:49:04 +0100

    Input-Methode für FutureAnalysis implementiert

--- src/app/models/implementation/Analysis/FutureAnalysis.java
+++ src/app/models/implementation/Analysis/FutureAnalysis.java
@@ -5,6 +5,7 @@ import app.models.interfaces.Analysis.I_AnalysisEntry;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
+import java.util.ArrayList;
 
 /**
  * Created by Michi on 23.04.2017.
@@ -31,5 +32,9 @@ public class FutureAnalysis extends Analysis {
 
     @Override
     public void importFromXML(XMLStreamReader xmlReader) throws XMLStreamException {
+        ArrayList<String> futureAnalysisEntryArguments = new ArrayList<>();
+        futureAnalysisEntryArguments.add(xmlReader.getAttributeValue(0));
+        futureAnalysisEntryArguments.add(xmlReader.getAttributeValue(1));
+        this.addEntryWithProperties(futureAnalysisEntryArguments);
     }
 }
