4402a63c0d4a870cb9cf541e3bbe42a938bf1cf8
Michi Mößner <michi.moessner@gmx.de>
2017-06-03 17:44:46 +0100

    Export-Methode für StateAnalysis hinzugefügt

--- src/app/models/implementation/Analysis/StateAnalysis.java
+++ src/app/models/implementation/Analysis/StateAnalysis.java
@@ -1,5 +1,7 @@
 package app.models.implementation.Analysis;
 
+import app.models.interfaces.Analysis.I_AnalysisEntry;
+
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
@@ -11,7 +13,20 @@ public class StateAnalysis extends Analysis {
 
     @Override
     public void exportAsXML(XMLStreamWriter xmlWriter) throws XMLStreamException {
+        xmlWriter.writeStartElement("StateAnalysisProperties");
 
+        for(I_AnalysisEntry currentEntry : this.getEntries())
+        {
+            xmlWriter.writeCharacters("\t");
+            xmlWriter.writeStartElement("StateAnalysisEntry");
+            xmlWriter.writeAttribute("entryName",
+                    currentEntry.getEntryName());
+            xmlWriter.writeAttribute("description",
+                    currentEntry.getDescription());
+            xmlWriter.writeEndElement();
+        }
+
+        xmlWriter.writeEndElement();
     }
 
     @Override
