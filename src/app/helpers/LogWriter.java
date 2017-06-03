b68bf0514ac60c914503ac8b16c4f6c3bf6c6e82
Michi Mößner <michi.moessner@gmx.de>
2017-06-03 15:23:12 +0100

    Die Klasse LogWriter kann nun überall im Projekt benutzt werden. In der Main-Methode muss zu Beginn der File festgelegt werden. Anschließend kann man an jeder Stelle mit "LogWriter.write(STRING)" etwas protokollieren. Die Einträge werden mit dem aktuellen Datum und der aktuellen Uhrzeit versehen und je Projekt fortlaufend geführt. Vor jedem Schreiben wird überprüft, ob der File lesbar und schreibbar ist und ob noch genügend Festplattenplatz vorhanden ist. Sollte einer dieser Fälle nicht gegeben sein, wird eine Exception geworfen. Sollte ein File nicht existieren, so wird dieser zunächst angelegt.

--- src/app/helpers/LogWriter.java
+++ src/app/helpers/LogWriter.java
@@ -1,16 +1,28 @@
 package app.helpers;
 
+import app.models.implementation.Project;
+
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Erstellt von Michi am 05.05.2017.
  */
+
+/**
+ * Logmeldungen, die aufgrund eines IO-Fehlers nicht direkt in der Datei gespeichert werden können, gehen verloren.
+ */
+
 public final class LogWriter
 {
     private static String fileName = "";
     private static BufferedWriter fileWriter = null;
     private static boolean canWrite = false;
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMdd - HH:mm:ss");
 
     // Klasse soll nicht-instanziierbar sein
     private LogWriter(){
@@ -20,27 +32,73 @@ public final class LogWriter
         if (canWrite && fileWriter != null)
         {
             try{
-                fileWriter.write(toWrite);
-                fileWriter.newLine();
+                File logFile = new File(fileName);
+
+                //Exception wird geworfen, falls Datei nicht zugreifbar oder freier Festplattenplatz < 1MB
+                if (!logFile.canRead() || !logFile.canWrite() || logFile.getFreeSpace() < 1048576)
+                {
+                    throw new Exception();
+                }
+                else {
+                    //Datum und Uhrzeit eigentlichem Ereignis voranstellen
+                    fileWriter.write(dateFormat.format(new Date()));
+                    fileWriter.write(": ");
+                    fileWriter.write(toWrite);
+                    fileWriter.newLine();
+                }
             }
             catch (Exception ex){
-                //TODO MessageBox an User
+                //tritt auf, wenn Datei nicht erstellt oder geöffnet werden kann
+                InfoDialog errorDialog = new InfoDialog("Fehler beim Zugriff auf Logdatei", "Auf die Logdatei konnte nicht erfolgreich zugegriffen werden. Sie können ANTool trotzdem weiterbenutzen.");
             }
+        }
+        else{
+            InfoDialog errorDialog = new InfoDialog("Fehler beim Zugriff auf Logdatei", "Auf die Logdatei konnte nicht erfolgreich zugegriffen werden. Sie können ANTool trotzdem weiterbenutzen.");
         }
     }
 
     private static boolean createLogFile(){
         try{
-            fileWriter = new BufferedWriter(new FileWriter(fileName));
-            //TODO Startinformation in Datei schreiben (z.B. Logfile für Projekt "Test":)
-            return true;
+            File logFile = new File(fileName);
+
+            if (!logFile.exists()) {
+                logFile.createNewFile();
+
+                //Exception wird geworfen, falls Datei nicht zugreifbar oder freier Festplattenplatz < 1MB
+                if (!logFile.canRead() || !logFile.canWrite() || logFile.getFreeSpace() < 1048576) {
+                    throw new Exception();
+                }
+                else {
+                    fileWriter = new BufferedWriter(new FileWriter(fileName));
+                    fileWriter.write("Logfile für Projekt \"" + Project.getInstance().getName() + "\":");
+                    fileWriter.newLine();
+                    return true;
+                }
+            }
+            else {
+                fileWriter = new BufferedWriter(new FileWriter(fileName, true));
+                return true;
+            }
         }
         catch (Exception ex) {
-            //TODO MessageBox als Info für User einfügen
-            System.out.println("Fehler beim Erstellen des Logfiles:\n" + ex.toString());
+            //tritt auf, wenn Datei nicht erstellt oder geöffnet werden kann
+            InfoDialog errorDialog = new InfoDialog("Fehler beim Zugriff auf Logdatei", "Auf die Logdatei konnte nicht erfolgreich zugegriffen werden. Sie können ANTool trotzdem weiterbenutzen.");
             return false;
         }
 
+    }
+
+    public static boolean closeFileWriter()
+    {
+        try{
+            fileWriter.close();
+            return true;
+        }
+        catch (Exception ex) {
+            //tritt auf, wenn Datei nicht geschlossen werden kann
+            InfoDialog errorDialog = new InfoDialog("Fehler beim Schließen auf Logdatei", "Die Logdatei konnte nicht erfolgreich geeschlossen werden. Möglicherweise wurden nicht alle Daten erfolgreich gespeichert.");
+            return false;
+        }
     }
 
     public static String getFileName(){
