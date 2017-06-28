91b3288a08b11e6ec512291d9428d568969ca04e
Michi Mößner <michi.moessner@gmx.de>
2017-06-28 22:38:45 +0100

    Unit-Tests hinzugefügt

--- src/app/tests/app/controller/CostEstimationTest.java
+++ src/app/tests/app/controller/CostEstimationTest.java
@@ -1,9 +1,12 @@
 package app.controller;
 
+import app.model.interfaces.CostEstimation.I_Classification;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,26 +15,135 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class CostEstimationTest {
     private CostEstimation _costEstimation;
-    private Method _testCalculateMenMonths;
-    private static String METHOD_NAME = "performCostEstimation";
-    private Class[] parameterTypes;
-    private Object[] parameters;
 
-    @BeforeAll
+    // test method: calculateMenMonths(double functionPoints)
+    private Method _testCalculateMenMonths;
+    private static String CALCULATE_MEN_MONTH_NAME = "calculateMenMonths";
+    private Class[] parameterTypes_CALCULATE_MEN_MONTH;
+    private Object[] parameters_CALCULATE_MEN_MONTH;
+
+    // test method: calculateFunctionTypesSums(I_Classification classification)
+    private Method _testCalculateFunctionTyesSums;
+    private static String CALCULATE_FUNCTION_TYPES_SUMS_NAME = "calculateFunctionTypesSums";
+    private Class[] parameterTypes_CALCULATE_FUNCTION_TYPES_SUMS;
+    private Object[] parameters_CALCULATE_FUNCTION_TYPES_SUMS;
+
+
+    @BeforeEach
     public void setUp() throws Exception {
         _costEstimation = new CostEstimation();
-        parameterTypes = new  Class[1];
-        parameterTypes[0] = java.lang.String.class;
-        _testCalculateMenMonths = _costEstimation.getClass().getDeclaredMethod(METHOD_NAME, parameterTypes);
+
+        // test method: calculateMenMonths(double functionPoints)
+        parameterTypes_CALCULATE_MEN_MONTH = new Class[1];
+        parameterTypes_CALCULATE_MEN_MONTH[0] = double.class;
+        _testCalculateMenMonths = _costEstimation.getClass().getDeclaredMethod(CALCULATE_MEN_MONTH_NAME, parameterTypes_CALCULATE_MEN_MONTH);
         _testCalculateMenMonths.setAccessible(true);
-        parameters = new Object[1];
+        parameters_CALCULATE_MEN_MONTH = new Object[1];
+
+        // test method: calculateFunctionTypesSums(I_Classification classification)
+        parameterTypes_CALCULATE_FUNCTION_TYPES_SUMS = new  Class[1];
+        parameterTypes_CALCULATE_FUNCTION_TYPES_SUMS[0] = app.model.implementation.CostEstimation.Classification.class;
+        _testCalculateFunctionTyesSums = _costEstimation.getClass().getDeclaredMethod(CALCULATE_MEN_MONTH_NAME, parameterTypes_CALCULATE_MEN_MONTH);
+        _testCalculateFunctionTyesSums.setAccessible(true);
+        parameters_CALCULATE_FUNCTION_TYPES_SUMS = new Object[1];
     }
 
+    // 4,68 < 50
     @Test
-    public void set_testCalculateMenMonths() throws Exception {
-        parameters[0] = 233.0;
-        double result = (Double) _testCalculateMenMonths.invoke(_costEstimation, parameters);
+    public void testCalculateMenMonths0() throws Exception {
+        parameters_CALCULATE_MEN_MONTH[0] = 4.68;
+        double result = (Double) _testCalculateMenMonths.invoke(_costEstimation, parameters_CALCULATE_MEN_MONTH);
+        assertEquals(result, 26.24);
+    }
+
+    // 50 <= 233,0 <= 2900
+    @Test
+    public void testCalculateMenMonths1() throws Exception {
+        parameters_CALCULATE_MEN_MONTH[0] = 233.0;
+        double result = (Double) _testCalculateMenMonths.invoke(_costEstimation, parameters_CALCULATE_MEN_MONTH);
         assertEquals(result, 15.98);
     }
 
+    // 50 <= 233,0 <= 2900
+    @Test
+    public void testCalculateMenMonths2() throws Exception {
+        parameters_CALCULATE_MEN_MONTH[0] = 378.0;
+        double result = (Double) _testCalculateMenMonths.invoke(_costEstimation, parameters_CALCULATE_MEN_MONTH);
+        assertEquals(result, 26.24);
+    }
+
+    // 3576,49 < 2900
+    @Test
+    public void testCalculateMenMonths3() throws Exception {
+        parameters_CALCULATE_MEN_MONTH[0] = 3576.49;
+        double result = (Double) _testCalculateMenMonths.invoke(_costEstimation, parameters_CALCULATE_MEN_MONTH);
+        assertEquals(result, 26.39);
+    }
+
+    //
+    @Test
+    public void testCalculateFunctionTypesSums() throws Exception {
+        I_Classification classification = (I_Classification) new Classification();
+        // Die ersten drei Einträge sind bei dieser Methode nicht relevant, weshalb hier jeweils "Test" als Platzhalter gewählt wurde
+        // Die klassifizierten Anforderungen entsprechen in der Anzahl der von uns im 3. Semester angefertigten Aufwandsschätzung als Teil der Spezifikation
+        // das bedeutet:
+        // EI: 22 x einfach, 1 x mittel, 0 x komplex
+        // EO: 8 x einfach, 2 x mittel, 0 x komplex
+        // EQ: 3 x einfach, 0 x mittel, 0 x komplex
+        // ILF: 0 x einfach, 0 x mittel, 6 x komplex
+        // ELF: 0 x einfach, 0 x mittel, 1 x komplex
+        // erwartetes Ergebnis: 221
+
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Eingabedaten (EI)"); add("mittel");}});
+
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("mittel");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Ausgabedaten (EO)"); add("mittel");}});
+
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Abfragen (EQ)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Abfragen (EQ)"); add("einfach");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Abfragen (EQ)"); add("einfach");}});
+
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Datenbestände (ILF)"); add("komplex");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Datenbestände (ILF)"); add("komplex");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Datenbestände (ILF)"); add("komplex");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Datenbestände (ILF)"); add("komplex");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Datenbestände (ILF)"); add("komplex");}});
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Datenbestände (ILF)"); add("komplex");}});
+
+        classification.addEntryWithProperties(new ArrayList<String>() {{add("Test"); add("Test"); add("Test"); add("Referenzdaten (ELF)"); add("komplex");}});
+
+        parameters_CALCULATE_FUNCTION_TYPES_SUMS[0] = classification;
+        int result = (Integer) _testCalculateFunctionTyesSums.invoke(_costEstimation, parameters_CALCULATE_MEN_MONTH);
+        assertEquals(result, 221);
+    }
 }
---
+++ src/app/tests/app/helpers/ValidateInputTest.java
@@ -0,0 +1,281 @@
+package app.helpers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Michi on 28.06.2017.
+ */
+class ValidateInputTest {
+    private ValidateInput _validateInput;
+
+    // test method: isValidPlainString(String input)
+    private Method _testIsValidPlainString;
+    private static String IS_VALID_PLAIN_STRING_NAME = "isValidPlainString";
+    private Class[] parameterTypes_IS_VALID_PLAIN_STRING;
+    private Object[] parameters_IS_VALID_PLAIN_STRING;
+
+    // test method: isValidDate(String input)
+    private Method _testIsValidDate;
+    private static String IS_VALID_DATE_NAME = "isValidDate";
+    private Class[] parameterTypes_IS_VALID_DATE;
+    private Object[] parameters_IS_VALID_DATE;
+
+    // test method: isValidPhoneNumber(String input)
+    private Method _testIsValidPhoneNumber;
+    private static String IS_VALID_PHONE_NUMBER_NAME = "isValidPhoneNumber";
+    private Class[] parameterTypes_IS_VALID_PHONE_NUMBER;
+    private Object[] parameters_IS_VALID_PHONE_NUMBER;
+
+    // test method: isValidPlz(String input)
+    private Method _testIsValidPlz;
+    private static String IS_VALID_PLZ_NAME = "isValidPlz";
+    private Class[] parameterTypes_IS_VALID_PLZ;
+    private Object[] parameters_IS_VALID_PLZ;
+
+    // test method: isValidEmailAddress(String input)
+    private Method _testIsValidEmailAddress;
+    private static String IS_VALID_EMAIL_ADDRESS_NAME = "isValidEmailAddress";
+    private Class[] parameterTypes_IS_VALID_EMAIL_ADDRESS;
+    private Object[] parameters_IS_VALID_EMAIL_ADDRESS;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        _validateInput = new ValidateInput();
+
+        // test method: isValidPlainString(String input)
+        parameterTypes_IS_VALID_PLAIN_STRING = new Class[1];
+        parameterTypes_IS_VALID_PLAIN_STRING[0] = java.lang.String.class;
+        _testIsValidPlainString = _validateInput.getClass().getDeclaredMethod(IS_VALID_PLAIN_STRING_NAME, parameterTypes_IS_VALID_PLAIN_STRING);
+        _testIsValidPlainString.setAccessible(true);
+        parameters_IS_VALID_PLAIN_STRING = new Object[1];
+
+        // test method: isValidDate(String input)
+        parameterTypes_IS_VALID_DATE = new Class[1];
+        parameterTypes_IS_VALID_DATE[0] = java.lang.String.class;
+        _testIsValidDate = _validateInput.getClass().getDeclaredMethod(IS_VALID_DATE_NAME, parameterTypes_IS_VALID_DATE);
+        _testIsValidDate.setAccessible(true);
+        parameters_IS_VALID_DATE = new Object[1];
+
+        // test method: isValidPhoneNumber(String input)
+        parameterTypes_IS_VALID_PHONE_NUMBER = new Class[1];
+        parameterTypes_IS_VALID_PHONE_NUMBER[0] = java.lang.String.class;
+        _testIsValidPhoneNumber = _validateInput.getClass().getDeclaredMethod(IS_VALID_PHONE_NUMBER_NAME, parameterTypes_IS_VALID_PHONE_NUMBER);
+        _testIsValidPhoneNumber.setAccessible(true);
+        parameters_IS_VALID_PHONE_NUMBER = new Object[1];
+
+        // test method: isValidPlz(String input)
+        parameterTypes_IS_VALID_PLZ = new Class[1];
+        parameterTypes_IS_VALID_PLZ[0] = java.lang.String.class;
+        _testIsValidPlz = _validateInput.getClass().getDeclaredMethod(IS_VALID_PLZ_NAME, parameterTypes_IS_VALID_PLZ);
+        _testIsValidPlz.setAccessible(true);
+        parameters_IS_VALID_PLZ = new Object[1];
+
+        // test method: isValidEmailAddress(String input)
+        parameterTypes_IS_VALID_EMAIL_ADDRESS = new Class[1];
+        parameterTypes_IS_VALID_EMAIL_ADDRESS[0] = java.lang.String.class;
+        _testIsValidEmailAddress = _validateInput.getClass().getDeclaredMethod(IS_VALID_EMAIL_ADDRESS_NAME, parameterTypes_IS_VALID_EMAIL_ADDRESS);
+        _testIsValidEmailAddress.setAccessible(true);
+        parameters_IS_VALID_EMAIL_ADDRESS = new Object[1];
+    }
+
+    @Test
+    public void testIsValidPlainString0() throws Exception {
+        parameters_IS_VALID_PLAIN_STRING[0] = "123456789";
+        boolean result = (boolean) _testIsValidPlainString.invoke(_validateInput, parameters_IS_VALID_PLAIN_STRING);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPlainString1() throws Exception {
+        parameters_IS_VALID_PLAIN_STRING[0] = "test";
+        boolean result = (boolean) _testIsValidPlainString.invoke(_validateInput, parameters_IS_VALID_PLAIN_STRING);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidPlainString2() throws Exception {
+        parameters_IS_VALID_PLAIN_STRING[0] = "test123";
+        boolean result = (boolean) _testIsValidPlainString.invoke(_validateInput, parameters_IS_VALID_PLAIN_STRING);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidDate0() throws Exception {
+        parameters_IS_VALID_DATE[0] = "01/01/1970";
+        boolean result = (boolean) _testIsValidDate.invoke(_validateInput, parameters_IS_VALID_DATE);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidDate1() throws Exception {
+        parameters_IS_VALID_DATE[0] = "01/01/70";
+        boolean result = (boolean) _testIsValidDate.invoke(_validateInput, parameters_IS_VALID_DATE);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidDate2() throws Exception {
+        parameters_IS_VALID_DATE[0] = "01.01. 1970";
+        boolean result = (boolean) _testIsValidDate.invoke(_validateInput, parameters_IS_VALID_DATE);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidDate3() throws Exception {
+        parameters_IS_VALID_DATE[0] = "1/1/70";
+        boolean result = (boolean) _testIsValidDate.invoke(_validateInput, parameters_IS_VALID_DATE);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidDate4() throws Exception {
+        parameters_IS_VALID_DATE[0] = "1.1.70";
+        boolean result = (boolean) _testIsValidDate.invoke(_validateInput, parameters_IS_VALID_DATE);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidDate5() throws Exception {
+        parameters_IS_VALID_DATE[0] = "1.1.1970";
+        boolean result = (boolean) _testIsValidDate.invoke(_validateInput, parameters_IS_VALID_DATE);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidDate6() throws Exception {
+        parameters_IS_VALID_DATE[0] = "01.01.1970";
+        boolean result = (boolean) _testIsValidDate.invoke(_validateInput, parameters_IS_VALID_DATE);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber0() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "+49 123 456789";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber1() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "+491234567891011";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber2() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "+4912345";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber3() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "012345/33";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber4() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "+49123456789";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber5() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "0123456789";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber6() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "+491234567-8910";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidPhoneNumber7() throws Exception {
+        parameters_IS_VALID_PHONE_NUMBER[0] = "01234567-891011";
+        boolean result = (boolean) _testIsValidPhoneNumber.invoke(_validateInput, parameters_IS_VALID_PHONE_NUMBER);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidPlz0() throws Exception {
+        parameters_IS_VALID_PLZ[0] = "123456";
+        boolean result = (boolean) _testIsValidPlz.invoke(_validateInput, parameters_IS_VALID_PLZ);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPlz1() throws Exception {
+        parameters_IS_VALID_PLZ[0] = "1234a";
+        boolean result = (boolean) _testIsValidPlz.invoke(_validateInput, parameters_IS_VALID_PLZ);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPlz2() throws Exception {
+        parameters_IS_VALID_PLZ[0] = "1234";
+        boolean result = (boolean) _testIsValidPlz.invoke(_validateInput, parameters_IS_VALID_PLZ);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidPlz3() throws Exception {
+        parameters_IS_VALID_PLZ[0] = "12345";
+        boolean result = (boolean) _testIsValidPlz.invoke(_validateInput, parameters_IS_VALID_PLZ);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidPlz4() throws Exception {
+        parameters_IS_VALID_PLZ[0] = "06789";
+        boolean result = (boolean) _testIsValidPlz.invoke(_validateInput, parameters_IS_VALID_PLZ);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidEmailAddress0() throws Exception {
+        parameters_IS_VALID_EMAIL_ADDRESS[0] = "max.mustermann@mail.ungueltig";
+        boolean result = (boolean) _testIsValidEmailAddress.invoke(_validateInput, parameters_IS_VALID_EMAIL_ADDRESS);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidEmailAddress1() throws Exception {
+        parameters_IS_VALID_EMAIL_ADDRESS[0] = "max.mustermann.de";
+        boolean result = (boolean) _testIsValidEmailAddress.invoke(_validateInput, parameters_IS_VALID_EMAIL_ADDRESS);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testIsValidEmailAddress2() throws Exception {
+        parameters_IS_VALID_EMAIL_ADDRESS[0] = "max@mustermann.de";
+        boolean result = (boolean) _testIsValidEmailAddress.invoke(_validateInput, parameters_IS_VALID_EMAIL_ADDRESS);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidEmailAddress3() throws Exception {
+        parameters_IS_VALID_EMAIL_ADDRESS[0] = "max.mustermann@mail.de";
+        boolean result = (boolean) _testIsValidEmailAddress.invoke(_validateInput, parameters_IS_VALID_EMAIL_ADDRESS);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testIsValidEmailAddress4() throws Exception {
+        parameters_IS_VALID_EMAIL_ADDRESS[0] = "max.mustermann@mail.net";
+        boolean result = (boolean) _testIsValidEmailAddress.invoke(_validateInput, parameters_IS_VALID_EMAIL_ADDRESS);
+        assertTrue(result);
+    }
+}
