bee7b5875ba9f921361e55e5ba7d224168c84998
Michi Mößner <michi.moessner@gmx.de>
2017-07-02 16:21:29 +0100

    Vorlage XmlImport-Test für Julius

---
+++ src/app/tests/app/helpers/importExport/XmlImporterTest.java
@@ -0,0 +1,71 @@
+package app.helpers.importExport;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Testklasse für {@link XmlImporter}
+ */
+class XmlImporterTest {
+
+    /**
+     * Testmethode für {@link XmlImporter#importXml()}
+     * Erwartetes Ergebnis:
+     *
+     * @throws Exception Wird geworfen, falls Test nicht ausführbar
+     */
+    @Test
+    public void testImportXML0() throws Exception
+    {
+        //assertEquals() / assertFalse() / assertTrue()
+    }
+
+    /**
+     * Testmethode für {@link XmlImporter#importXml()}
+     * Erwartetes Ergebnis:
+     *
+     * @throws Exception Wird geworfen, falls Test nicht ausführbar
+     */
+    @Test
+    public void testImportXML1() throws Exception
+    {
+        //assertEquals() / assertFalse() / assertTrue()
+    }
+
+    /**
+     * Testmethode für {@link XmlImporter#importXml()}
+     * Erwartetes Ergebnis:
+     *
+     * @throws Exception Wird geworfen, falls Test nicht ausführbar
+     */
+    @Test
+    public void testImportXML2() throws Exception
+    {
+        //assertEquals() / assertFalse() / assertTrue()
+    }
+
+    /**
+     * Testmethode für {@link XmlImporter#importXml()}
+     * Erwartetes Ergebnis:
+     *
+     * @throws Exception Wird geworfen, falls Test nicht ausführbar
+     */
+    @Test
+    public void testImportXML3() throws Exception
+    {
+        //assertEquals() / assertFalse() / assertTrue()
+    }
+
+    /**
+     * Testmethode für {@link XmlImporter#importXml()}
+     * Erwartetes Ergebnis:
+     *
+     * @throws Exception Wird geworfen, falls Test nicht ausführbar
+     */
+    @Test
+    public void testImportXML4() throws Exception
+    {
+        //assertEquals() / assertFalse() / assertTrue()
+    }
+}
